c9bc63c85afd76eac8a1cd01a46969027e97d76b
용석 박 <4up456@naver.com>
2022-01-08 18:56:16 +0900

    랜덤으로 아이템 뿌리기 성공!

--- dreamcoding/chapter2/carrot/main.js
+++ dreamcoding/chapter2/carrot/main.js
@@ -7,18 +7,19 @@ const wonModal = document.querySelector('.won-modal');
 const gameArea = document.querySelector('.game-area');
 const carrotCounter = document.querySelector('.carrot-counter');
 
-let carrot = 4;
+let carrot = 10;
 
 function playGame() {
   playBtn.textContent = '■';
   playTimer();
-  // 아이템을 셋팅한다()
   setItems();
-  carrot = 4;
-  gameArea.addEventListener('click', (event) => {
-    rolePlay(event);
-  });
+  carrot = 10;
+  carrotCounter.textContent = carrot;
 }
+
+gameArea.addEventListener('click', (event) => {
+  rolePlay(event);
+});
 
 function rolePlay(event) {
   // 벌레를 클릭한다 -> 게임에서 패배한다.
@@ -38,15 +39,55 @@ function rolePlay(event) {
 }
 function setItems() {
   gameArea.innerHTML = `
-  <img class="carrot" src="./img/carrot.png" alt="당근">
-    <img class="carrot" src="./img/carrot.png" alt="당근">
-    <img class="carrot" src="./img/carrot.png" alt="당근">
     <img class="carrot" src="./img/carrot.png" alt="당근">
     <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
     <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
     <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
     <img class="bug" src="./img/bug.png" alt="벌레">
-    <img class="bug" src="./img/bug.png" alt="벌레">`;
+    <img class="carrot" src="./img/carrot.png" alt="당근">
+    <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
+    <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
+    <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
+    <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
+    <img class="bug" src="./img/bug.png" alt="벌레">
+    <img class="carrot" src="./img/carrot.png" alt="당근">
+    <img class="bug" src="./img/bug.png" alt="벌레">
+  `;
+  mixPosition('.carrot');
+  mixPosition('.bug');
+}
+gameArea.addEventListener('mouseover', (event) => {
+  if (event.target.getAttribute('class') !== 'game-area') {
+    event.target.style.transform = `${event.target.style.transform} scale(1.1)`;
+  }
+});
+gameArea.addEventListener('mouseout', (event) => {
+  if (event.target.getAttribute('class') !== 'game-area') {
+    let transform = event.target.style.transform;
+    let newTransform = transform.slice(0, transform.indexOf('scale'));
+    event.target.style.transform = newTransform;
+    // 이벤트 요소에 직접 접근하지 않고 변수에 담아서 접근하여 할당하면 정상적으로 작동하지 않을 수도 있다~!
+    // transform = newTransform;
+  }
+});
+
+function getRandom(num) {
+  return Math.random() * num;
+}
+function mixPosition(target) {
+  const items = document.querySelectorAll(target);
+  items.forEach((item) => {
+    const x = getRandom(1250);
+    const y = getRandom(200);
+    item.style.transform = `translate(${x}px, ${y}px)`;
+  });
 }
 
 // 맨처음 게임을 시작하고, 멈추는 경우
